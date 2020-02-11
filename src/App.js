72014e1daa8e97ce6a038c8035aaaf64e328bf8e
Fahmid Anabi <me.anabi@gmail.com>
2020-02-11 12:01:07 +0600

    init

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h1", {}, "Adopt Me")
-  );
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed)
+  ]);
 };
-ReactDOM.render(
 
-
-  React.createElement(App),
-  document.getElementById("root")
-);
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me"),
+    React.createElement(Pet, { name: "Joker", animal: "Dog", breed: "Husky" }),
+    React.createElement(Pet, { name: "Jack", animal: "Cat", breed: "Turkey" }),
+    React.createElement(Pet, { name: "Joshim", animal: "Bat", breed: "Batman" })
+  ]);
+};
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
